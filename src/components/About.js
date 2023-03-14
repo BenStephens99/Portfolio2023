c400034346d4900e1831b9f1deb29b7280648251
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2023-03-14 22:03:34 +0000

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -18,18 +18,27 @@ function About() {
                 </div>
 
                 <div className="bio box">
-                    <h1><HackerEffect text="About me"/></h1>
-                    <p>My name is Ben and I am a recent graduate from the University of Brighton.
-                        I am excited to begin my journey as a developer and am eager to apply my
-                        skills and knowledge to create engaging and user-friendly websites and
-                        applications.</p>
-                    <p>On my portfolio website, you will find a selection of my most recent projects,
-                        along with detailed explanations of the technologies and techniques I used to create them.
+                    <h1><HackerEffect text="About me" /></h1>
+                   
+                    <p>Hello and welcome to my website! I am a computer science graduate and 
+                        passionate about front-end development. I have always been fascinated
+                         by the way websites and applications are built and designed, and I am excited 
+                         to turn that passion into a career. </p>
+
+                    <p> Throughout my studies, I gained extensive experience in programming languages such 
+                        as HTML, CSS, JavaScript, and React. I am skilled in creating responsive and user-friendly 
+                        websites that provide an engaging experience for users. </p>
+
+                     <p>   
+                        My experience also includes working on several projects, both independently 
+                        and collaboratively, which has allowed me to develop strong problem-solving skills 
+                        and an eye for detail. I am always eager to learn and keep up-to-date with the latest 
+                        trends and technologies in front-end development.
                     </p>
                 </div>
 
                 <div className="qualifications box">
-                    <h3><HackerEffect text="Qualifications"/></h3>
+                    <h3><HackerEffect text="Qualifications" /></h3>
                     <div className="qual">
                         <p className="accent"> Computer Science for Games BSc Hons</p>
                         <p >First Class Honours</p>
@@ -44,7 +53,7 @@ function About() {
                 </div>
 
                 <div className="map box">
-                <iframe title="map" width="100%" height="100%" src="https://www.openstreetmap.org/export/embed.html?bbox=-4.510024484023259%2C48.95858066440977%2C4.251572195664242%2C52.63973017532399&amp;layer=mapnik&amp;marker=50.835432306955276%2C-0.12922614417948353"></iframe>
+                    <iframe title="map" width="100%" height="100%" src="https://www.openstreetmap.org/export/embed.html?bbox=-4.510024484023259%2C48.95858066440977%2C4.251572195664242%2C52.63973017532399&amp;layer=mapnik&amp;marker=50.835432306955276%2C-0.12922614417948353"></iframe>
                 </div>
 
             </div>
